aca2f2f59269b5e30d39285692b47ce485556737
thekennedys <thekennedys@users.noreply.github.com>
2015-10-26 11:38:35 +0100

    test af github

--- footer.php
+++ footer.php
@@ -4,3 +4,5 @@
 <area shape="rect" coords="1500,101,1900,370" href="https://www.facebook.com/pages/Grimhøj-Motocross-Klub/188563657824722?fref=ts" 
 target="_blank" alt="Facebook">
 </map>
+
+<?php // Dette er en kommentar?>
