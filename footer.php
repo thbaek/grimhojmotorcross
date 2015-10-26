8c6801b7c74e2f6e1e98e94bf36afd0f97943542
kaw0808 <kawther_sk@hotmail.com>
2015-10-26 11:37:23 +0100

    test
    ændring af footer

--- footer.php
+++ footer.php
@@ -4,3 +4,5 @@
 <area shape="rect" coords="1500,101,1900,370" href="https://www.facebook.com/pages/Grimhøj-Motocross-Klub/188563657824722?fref=ts" 
 target="_blank" alt="Facebook">
 </map>
+
+<h1>Ændring af kawther<h1> 
